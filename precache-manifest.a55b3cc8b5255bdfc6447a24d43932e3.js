d274feb305cd4aba8b3e23828d26b313b6c5aee2
johan-avila <johan-avila@outlook.com>
2020-05-16 00:37:42 -0500

    Updates

---
+++ precache-manifest.a55b3cc8b5255bdfc6447a24d43932e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b2127efdbaa5d91db8b33b449556db",
+    "url": "/rickandmortyapi/index.html"
+  },
+  {
+    "revision": "2b49a777a207fdce09e7",
+    "url": "/rickandmortyapi/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4ab95f0232c34ce0b0fe",
+    "url": "/rickandmortyapi/static/css/main.08345742.chunk.css"
+  },
+  {
+    "revision": "2b49a777a207fdce09e7",
+    "url": "/rickandmortyapi/static/js/2.619cd0a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rickandmortyapi/static/js/2.619cd0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ab95f0232c34ce0b0fe",
+    "url": "/rickandmortyapi/static/js/main.0840d115.chunk.js"
+  },
+  {
+    "revision": "f75f5d33451f8b7ddc8c",
+    "url": "/rickandmortyapi/static/js/runtime-main.5a824765.js"
+  }
+]);
